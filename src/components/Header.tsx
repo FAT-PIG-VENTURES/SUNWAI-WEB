d247079e1c3928f266c800f425dd52646ffe5dbe
RITAM GHOSH <ritamghosh195@gmail.com>
2026-08-18 14:13:07 +0530

    Fix header logo positioning and alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,13 @@ export default function Header() {
     <header className="header">
       <div className="container header-container">
         <Link href="/" className="logo">
-          <Image src="/sunwai-icon.png" alt="Sunwai" width={36} height={36} style={{ borderRadius: '8px' }} />
+          <Image
+            src="/sunwai-icon.png"
+            alt="Sunwai"
+            width={32}
+            height={32}
+            style={{ borderRadius: '7px', flexShrink: 0 }}
+          />
           <span className="logo-latin">
             sunwa<span style={{ color: 'var(--stamp)' }}>i</span>
           </span>
